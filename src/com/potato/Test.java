c9ed905a1f43343ee10ed03869b9dc40ab548b4f
tankaohuangfeng <tankaohuangfeng@foxmail.com>
2021-02-07 21:57:14 +0800

    Some improve

--- src/com/potato/Test.java
+++ src/com/potato/Test.java
@@ -9,9 +9,9 @@ public class Test
 	public static void main(String[] args)
 	{
 		System.out.println("欢迎使用本收银台");
-		Output.showCommodity();
-		Input.purchasedItemsInput();
-		MoneyManager.manager();
-		Output.billPlease(MoneyManager.lumpSum);
+		Output.showCommodity();  // 显示商品
+		Input.purchasedItemsInput();  // 输入要购买的物品
+		MoneyManager.manager();  // 结算总额
+		Output.billPlease(MoneyManager.lumpSum);  // 结账
 	}
 }
